4a0df37743ed57113d2cf575e8cf624eab05f15c
Viktor Pukhnichenko <victorpuhnichenko@gmail.com>
2019-09-16 00:18:45 +0300

    Updates

---
+++ precache-manifest.0fac5787f7b93a8fb8a3168d2b25260d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9042b7d509f84850621d06d060473781",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "67a7f333e5a95983301c",
+    "url": "/react_tabs/static/css/main.b61e7e4c.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_tabs/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "67a7f333e5a95983301c",
+    "url": "/react_tabs/static/js/main.679ec526.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
